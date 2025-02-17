5db91ab6fd27c7a4517050e7f807a09e785c38a9
kiles <kilesosu@gmail.com>
2025-02-17 15:21:04 +0500

    Реализована серверная часть для обработки запросов от ESP

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -5,6 +5,8 @@ import com.example.demo.websocket.WebSocketServer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -20,5 +22,11 @@ public class GreetingController {
     public String bumbumbam(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "xindex";
+    }
+
+    @GetMapping("/esp")
+    public String esp(@RequestParam(name = "uid", required = false, defaultValue = "Error") String name, Model model) {
+        model.addAttribute("uid", name);
+        return "esp";
     }
 }
