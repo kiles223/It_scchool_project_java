80bed14375141990947042d42534f8e6f295afd3
kiles <kilesosu@gmail.com>
2025-02-23 19:50:07 +0500

    Добавил конфигуратор для обратчика ресурных файлов и немного навел порядок в файлах

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,17 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.websocket.WebSocketConfig;
-import com.example.demo.websocket.WebSocketServer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingController {
-    int i = 0;
+
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
@@ -19,15 +14,14 @@ public class GreetingController {
         return "greeting";
     }
     @GetMapping("/bum")
-    public String bumbumbam(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "xindex";
+    public String bumbumbam() {
+        return "index";
     }
 
     @GetMapping("/esp")
     public String esp(@RequestParam(name = "uid", required = false, defaultValue = "Error") String name, Model model) {
         model.addAttribute("uid", name);
-        return "esp";
+        return "ESP";
     }
 
 }
