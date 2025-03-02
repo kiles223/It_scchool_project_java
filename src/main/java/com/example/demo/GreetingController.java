8036b27165f0e13191528a3a0ac1bdd633e65a91
arsen <arsen.abdulgazin@gmail.com>
2025-03-02 20:35:43 +0500

    Изменения от 24.02.25

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.model.House;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +14,11 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
-    @GetMapping("/bum")
-    public String bumbumbam() {
+    @GetMapping("/")
+    public String bumbumbam(Model model) {
+        House test = new House("дом №13 «тестовый» ","1300.00","поздравляю");
+        model.addAttribute("house", test);
+
         return "index";
     }
 
---
+++ src/main/java/com/example/demo/model/House.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+/*
+Что должно быть у дома ( его описания )
+Название ( ДОМ №... "....." )
+Цена за сутки ( ..... ₽ )
+Описание
+
+Фотографии
+Календарь и даты бронирования, когда он занят
+Отображение на карте?????
+ */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class House {
+    String name;
+    String price;
+    String description;
+
+}
